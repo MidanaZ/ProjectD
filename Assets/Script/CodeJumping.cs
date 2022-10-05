72ce84675a54452701bba728d88cce8e6d6d0762
TrungDang <trungdn@ebizworld.com.vn>
2022-10-05 20:31:32 +0700

    update

---
+++ Assets/Script/CodeJumping.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeJumping : MonoBehaviour
+{
+    public NewPlayerMovement move;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
